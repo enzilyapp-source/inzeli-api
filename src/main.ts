163ec70a1bf2c444047e70c22e8d35cf7cd6cbcf
enzilyapp-source <enzilyapp@gmail.com>
2026-01-21 17:45:23 +0300

    chore: fix gitignore and listen on render port

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,8 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 3000;
-  await app.listen(port, '127.0.0.1');
-  console.log(`🚀 API running on http://127.0.0.1:${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 API running on http://0.0.0.0:${port}`);
 }
 bootstrap();
 //main.ts
